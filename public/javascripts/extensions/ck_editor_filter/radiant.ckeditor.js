f35ce42f07923d4ae347ed1a92829c187c9cad72
Benny Degezelle <benny@gorilla-webdesign.be>
2011-02-10 15:22:08 +0100

    now plays nice with paperclipped

--- public/javascripts/extensions/ck_editor_filter/radiant.ckeditor.js
+++ public/javascripts/extensions/ck_editor_filter/radiant.ckeditor.js
@@ -1,36 +1,68 @@
 function instantiateCkEditor(partIndex){
-	CKEDITOR.config.startupOutlineBlocks = true;
-	CKEDITOR.config.colorButton_enableMore = false;
-	CKEDITOR.config.protectedSource.push( /<r:([\S]+)*>.*<\/r:\1>/g );
-	CKEDITOR.config.protectedSource.push( /<r:[^>\/]*\/>/g );
+	CKEDITOR.config.startupOutlineBlocks = true
+	CKEDITOR.config.colorButton_enableMore = false
+	CKEDITOR.config.protectedSource.push( /<r:([\S]+)*>.*<\/r:\1>/g )
+	CKEDITOR.config.protectedSource.push( /<r:[^>\/]*\/>/g )
+	CKEDITOR.config.extraPlugins = 'radiant_paperclipped,MediaEmbed'
 	CKEDITOR.config.toolbar =
 	[
 	    ['Source','-','Save','Preview','-','Templates'],
 	    ['Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
 	    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-	    ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
-	    '/',
+	    // ['Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField'],
+	    ['Image','RadiantPaperclipped', 'MediaEmbed', 'Table','HorizontalRule','SpecialChar','PageBreak'],
+			'/',
 	    ['Styles','Format'],
 	    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
 	    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote'],
 	    ['JustifyLeft','JustifyCenter','JustifyRight'],
 	    ['Link','Unlink','Anchor'],
-	    ['Image','Table','HorizontalRule','SpecialChar','PageBreak'],
 	    ['Maximize','-','About']
-	];
+	]
+	CKEDITOR.on('instanceReady',
+		function( evt ) {
+			var editor = evt.editor
+			var ck_holder = $("cke_" + editor.name)
+			if(ck_holder){
+				Droppables.add(ck_holder, {
+					accept: 'asset',
+					onDrop: function(element) {
+						var link = element.select('a.bucket_link')[0]
+						var classes = element.className.split(' ')
+						var tag_type = classes[0]
+						if(tag_type == 'image') {
+			        var tag = '<img src="'+ link.href +'" />'
+			      }
+			      else {
+			        var asset_id = element.id.split('_').last();
+			        var tag = '<a href="'+ link.href +'">'+ link.title +'</a>'
+			      }
+						var element = CKEDITOR.dom.element.createFromHtml(tag)
+						editor.insertElement(element)
+				  }
+				})
+			}
+    }
+	)
 	
-	var usedFilter = $('part_' + partIndex +'_filter_id');
+	var usedFilter = $('part_' + partIndex +'_filter_id')
 	if(usedFilter.value == 'CKEditor'){
-		putInEditor(partIndex);
+		putInEditor(partIndex)
 	}
+	
+	// Make asset bucket thumbnails draggable 
+  $$('div.resized').each(function(element){
+    new Draggable(element, { revert: true })
+    element.addClassName('move')
+  });
 }
 
 function toggleEditor(partIndex){
-	var filterId = $('part_' + partIndex + '_filter_id');
+	var filterId = $('part_' + partIndex + '_filter_id')
 	if(filterId.value == 'CKEditor'){
-		putInEditor(partIndex);
+		putInEditor(partIndex)
 	} else {
-		removeEditor(partIndex);
+		removeEditor(partIndex)
 	}
 }
 
@@ -40,9 +72,6 @@ function removeEditor(partIndex){
 }
 
 function putInEditor(partIndex){
-	var textarea = $('part_' + partIndex + '_content');
-	// replace radius tags with 
-	
-	CKEDITOR.replace(textarea);
+	var textarea = $('part_' + partIndex + '_content')
+	CKEDITOR.replace(textarea)
 }
-
