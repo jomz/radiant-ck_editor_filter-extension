c2c5ad05167df4e0d9ce6636893d04536eb50b0c
Benny Degezelle <hi@monkeypatch.be>
2014-01-20 11:20:24 +0100

    Bump version to 0.2.8

--- lib/radiant-ck_editor_filter-extension.rb
+++ lib/radiant-ck_editor_filter-extension.rb
@@ -1,5 +1,5 @@
 module RadiantCkEditorFilterExtension
-  VERSION     = "0.2.7"
+  VERSION     = "0.2.8"
   SUMMARY     = "CKEditor filter for Radiant CMS"
   DESCRIPTION = "Adds CKEditor as a filter"
   URL         = "https://github.com/jomz/radiant-ck_editor_filter-extension"
